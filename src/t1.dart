b7bee59de270cca736eade8baa6f65dd6529f2bb
cordis-dev <darius@adroiti.com>
2019-11-20 12:45:28 +0200

    Update t1.dart

--- src/t1.dart
+++ src/t1.dart
@@ -1,4 +1,19 @@
-if (x > y)
-  print("1");
-else ;
-  print("2");
+class Size {
+  final int inBytes;
+  const Size(this.inBytes);
+
+  @override
+  bool operator ==(Object other) => other is Size && other.inBytes == inBytes;
+
+  @override
+  int get hashCode => inBytes.hashCode;
+  
+  void f(int x, int y) {
+    print('debug: $x');
+	
+    if (x > y)
+		print("1");
+	else ;
+	print("2");
+  }
+}
